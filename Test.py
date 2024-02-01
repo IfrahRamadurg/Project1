a45c1c196ac43c24cdad0611facd1922fa88a97f
IfrahRamadurg <155607021+IfrahRamadurg@users.noreply.github.com>
2024-02-01 12:01:30 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -10,7 +10,7 @@ class TestSum(unittest.TestCase):
     """
     data=[20,30]
     result=summation(data)
-    self.assertEqual(result,50)
+    self.assertEqual(result,55)
 
 if __name__=='__main__':
   unittest.main()
